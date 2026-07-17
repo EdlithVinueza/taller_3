96c6b046e387fee82b6415498bcf4ae2555a0866
EdlithVinueza <eavinuezaz1@uce.edu.ec>
2026-07-17 08:47:58 -0500

    prueba_1

---
+++ Prueba/src/main/java/org/example/routers/FileReaderRouter.java
@@ -0,0 +1,30 @@
+package com.programacion.taller3.routers;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileReaderRouter extends RouteBuilder {
+
+    // Se mantiene el formato exacto de tu docente, pero apuntando a tu carpeta en
+    // Windows
+    @Value("${app.files.inbound:/home/edlith/tools/springai}")
+    String inboundPath;
+
+    @Override
+    public void configure() throws Exception {
+
+        String from = "file:%s?antInclude=*.pdf&delay=1000&move=procesados".formatted(inboundPath);
+
+        from(from)
+                .log("Archivo leido: ${header.CamelFileName}")
+                .bean("fileProcessor")
+                .bean("transformerProcessor")
+                .bean("embeddingProcessor");
+        ;
+    }
+}
---
+++ Prueba/src/main/java/org/example/services/EmbeddingProcessor.java
@@ -0,0 +1,31 @@
+package org.example.services;
+
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmbeddingProcessor {
+
+        @Autowired
+        VectorStore vectorStore;
+
+        @Autowired
+        EmbeddingModel embeddingModel;
+
+        public void procesar(List<Document> documents) {
+            System.out.println("EmbeddingProcessor::procesar documentos: " + documents.size());
+
+            System.out.println(embeddingModel);
+            System.out.println(vectorStore);
+
+            vectorStore.add(documents);
+        }
+    }
+
+
+
